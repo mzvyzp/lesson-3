d8a7631d5556df1b9d62d1a9872c9a3709e1abd4
mzviahin <mihanik.zp@gmail.com>
2025-02-02 21:49:54 +0100

    3rd task

---
+++ decision-tree.js
@@ -0,0 +1,10 @@
+let daysInCountry = 1062;
+if (daysInCountry < 183) {
+    console.log('You are a tourist');
+} else if (daysInCountry >= 183 && daysInCountry < 5 * 365) {
+    console.log('You are a temporary resident');
+} else  if (daysInCountry >= 5 * 365 && daysInCountry < 10 * 365) {
+    console.log('You are a permanent resident');
+} else {
+    console.log('You are a citizen');
+}
