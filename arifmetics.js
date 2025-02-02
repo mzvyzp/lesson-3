ae5e39b79320319a3d573b9a1209426c5988d0cd
mzviahin <mihanik.zp@gmail.com>
2025-02-02 20:01:42 +0100

    1st part in process

---
+++ arifmetics.js
@@ -0,0 +1,10 @@
+function arifmetics () {
+    const a = 5;
+    let b = 12;
+    const c = 1;
+    let d = 4;
+    console.log(a, b, c, d);
+    b = a + d;
+    console.log ("b", b);
+}
+arifmetics();
